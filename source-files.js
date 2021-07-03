6358c3c97607bdf1ad37b840db099b96cb61ef8d
realaravinth <realaravinth@batsense.net>
2021-07-03 16:49:31 +0000

    Deploying to gh-pages from  @ 612188bc78df908fc3f367c21e89a37164a5518f 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["actix_stateful_session"] = {"name":"","files":["lib.rs","session.rs"]};
+sourcesIndex["actix_stateful_session"] = {"name":"","files":["lib.rs","redis.rs","session.rs"]};
 createSourceSidebar();
